fef2fe03e17b58848209c8dfb22a49bf0381fa6d
NGT50 <129164295+NGT50@users.noreply.github.com>
2025-08-13 15:58:40 +0900

    Add null filtering to claim draft DTO

--- backend/src/main/java/com/patentsight/ai/dto/ClaimDraftDetails.java
+++ backend/src/main/java/com/patentsight/ai/dto/ClaimDraftDetails.java
@@ -1,6 +1,7 @@
 package com.patentsight.ai.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ClaimDraftDetails {
 
     @JsonProperty("log_id")
