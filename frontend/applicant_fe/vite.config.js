687adf27565e6f49c453a5682e6cbd2c1ec02be1
parkpark79 <102575038+parkpark79@users.noreply.github.com>
2025-08-15 19:27:22 +0900

    Update vite.config.js

--- frontend/applicant_fe/vite.config.js
+++ frontend/applicant_fe/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/applicant/',    // ★ 서브 경로 설정 추가
   css: {
     postcss: './postcss.config.js',
   },
