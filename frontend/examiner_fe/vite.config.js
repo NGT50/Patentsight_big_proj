bc2919eece7740b9e68e1010af6a48daa16506be
parkpark79 <102575038+parkpark79@users.noreply.github.com>
2025-08-15 19:25:27 +0900

    Update vite.config.js

--- frontend/examiner_fe/vite.config.js
+++ frontend/examiner_fe/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/examiner/',    // ★ 서브 경로 설정 추가
   css: {
     postcss: './postcss.config.js',
   },
