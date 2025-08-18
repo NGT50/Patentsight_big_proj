3224f552ea2e89f3fdda2245b213083d075fbc05
nana <nsw13579@naver.com>
2025-08-18 12:30:35 +0900

    Add missing List import in SecurityConfig

--- backend/src/main/java/com/patentsight/config/SecurityConfig.java
+++ backend/src/main/java/com/patentsight/config/SecurityConfig.java
@@ -26,6 +26,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
