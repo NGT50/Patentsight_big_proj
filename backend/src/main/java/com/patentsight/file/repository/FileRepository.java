1ca2646ac8f9d68c6b4205c24a4d33ae7222edf4
parkpark79 <102575038+parkpark79@users.noreply.github.com>
2025-08-24 14:05:40 +0900

    Update FileRepository.java

--- backend/src/main/java/com/patentsight/file/repository/FileRepository.java
+++ backend/src/main/java/com/patentsight/file/repository/FileRepository.java
@@ -3,5 +3,10 @@ package com.patentsight.file.repository;
 import com.patentsight.file.domain.FileAttachment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FileRepository extends JpaRepository<FileAttachment, Long> {
+
+    // ★ 추가된 부분: 특정 특허에 속한 파일만 조회
+    List<FileAttachment> findByPatent_PatentId(Long patentId);
 }
