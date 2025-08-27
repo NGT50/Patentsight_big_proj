e555bc1117f294b94d10a452a9351ce97cccfe80
chaeyoonseung <thestar4797@gmail.com>
2025-08-27 13:46:12 +0900

    mock유사

--- frontend/examiner_fe/src/pages/PatentReview.jsx
+++ frontend/examiner_fe/src/pages/PatentReview.jsx
@@ -512,7 +512,7 @@ useEffect(() => {
   setIsSearchingSimilarity(true);
   // 약간의 로딩 연출
   const t = setTimeout(() => {
-    setSimilarityResults(MOCK_SIMILAR_RESULTS);
+    setSimilarityResults(mockSimilarityResults);
     setIsSearchingSimilarity(false);
   }, 400);
   return () => clearTimeout(t);
